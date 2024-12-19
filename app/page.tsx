2f85bf52b29ca48aa8957fbe7b4971bdf5a2c180
indra-182 <mahadiindra2@gmail.com>
2024-12-20 00:46:06 +0700

    feat: change btn to switch toggle theme

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <section className='py-24'>
       <div className='container max-w-3xl'>
-        <h1 className='text-3xl font-bold'>Portfolio</h1>
+        <h1 className='text-3xl font-bold'></h1>
       </div>
     </section>
   )
--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -2,10 +2,10 @@
 
 import { useTheme } from 'next-themes'
 import React, { useEffect, useState } from 'react'
-import { Button } from './ui/button'
 import { MoonIcon, SunIcon } from 'lucide-react'
+import { Switch } from './ui/switch'
 
-const ThemeToggle = () => {
+const ThemeToggleSwitch = () => {
   const { setTheme, resolvedTheme } = useTheme()
   const [mounted, setMounted] = useState<boolean>(false)
 
@@ -14,23 +14,39 @@ const ThemeToggle = () => {
   }, [])
 
   if (!mounted) {
-    return null
+    return (
+      <div className='flex h-full w-full items-center justify-center'>
+        <div className='h-6 w-6 animate-spin rounded-full border-b-2 border-t-2 border-white'></div>
+      </div>
+    )
+  }
+
+  const handleThemeChange = () => {
+    setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
   }
 
   return (
-    <Button
-      size='sm'
-      variant='ghost'
-      onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
-    >
-      {resolvedTheme === 'dark' ? (
-        <SunIcon className='size-4 text-orange-300' />
-      ) : (
-        <MoonIcon className='size-4 text-sky-950' />
-      )}
-      <span className='sr-only'>Toggle Theme</span>
-    </Button>
+    <div className='flex items-center space-x-2'>
+      <SunIcon
+        className={`h-5 w-5 ${resolvedTheme === 'light' ? 'text-orange-300' : 'text-gray-400'}`}
+      />
+      <Switch
+        checked={resolvedTheme === 'dark'}
+        onCheckedChange={handleThemeChange}
+        className='relative inline-flex h-6 w-11 items-center rounded-full transition-transform duration-500 ease-in-out'
+      >
+        <span
+          aria-hidden='true'
+          className={`inline-block h-4 w-4 transform rounded-full bg-white shadow transition-transform duration-500 ease-in-out ${
+            resolvedTheme === 'dark' ? 'translate-x-5' : 'translate-x-0'
+          }`}
+        />
+      </Switch>
+      <MoonIcon
+        className={`h-5 w-5 ${resolvedTheme === 'dark' ? 'text-sky-950' : 'text-gray-400'}`}
+      />
+    </div>
   )
 }
 
-export default ThemeToggle
+export default ThemeToggleSwitch
