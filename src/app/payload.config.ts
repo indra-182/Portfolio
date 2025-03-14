4e7dd922de1bea4448f14eb9d4a839acbe0f902e
mahadi.indra <mahadi.indra@jatis.com>
2025-03-14 17:31:08 +0700

    feat : add payloadcms/storage-vercel-blob

--- src/app/payload.config.ts
+++ src/app/payload.config.ts
@@ -1,9 +1,11 @@
 import sharp from "sharp";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import { postgresAdapter } from "@payloadcms/db-postgres";
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 import { buildConfig } from "payload";
 import { Posts } from "./collections/Posts";
 import { Media } from "./collections/Media";
+
 
 export default buildConfig({
   // If you'd like to use Rich Text, pass your editor here
@@ -11,6 +13,20 @@ export default buildConfig({
 
   // Define and configure your collections in this array
   collections: [Posts, Media], // Add the Media collection here
+  plugins: [
+    vercelBlobStorage({
+      enabled: true, // Optional, defaults to true
+      // Specify which collections should use Vercel Blob
+      collections: {
+        media: true,
+        'media-with-prefix': {
+          prefix: 'my-prefix',
+        },
+      },
+      // Token provided by Vercel once Blob storage is added to your Vercel project
+      token: process.env.BLOB_READ_WRITE_TOKEN,
+    }),
+  ],
 
   // Your Payload secret - should be a complex and secure string, unguessable
   secret: process.env.PAYLOAD_SECRET || "",
